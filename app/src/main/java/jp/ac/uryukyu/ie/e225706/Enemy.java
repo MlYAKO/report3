dfb6594784219129bf1117dd2bf53d2fbfce54db
Kyosuke Kobashigawa <e225706@ie.u-ryukyu.ac.jp>
2022-11-25 22:02:57 +0900

    step1

--- app/src/main/java/jp/ac/uryukyu/ie/e225706/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225706/Enemy.java
@@ -8,12 +8,7 @@ package jp.ac.uryukyu.ie.e225706;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Enemy {
-    private String name;
-    private int hitPoint;
-    private int attack;
-    private boolean dead;
-
+public class Enemy extends LivingThing{
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
      * @param name モンスター名
@@ -21,62 +16,7 @@ public class Enemy {
      * @param attack モンスターの攻撃力
      */
     public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-    }
-
-    public String getname() {
-        return this.name;
-    }
-
-    public void setname(String name) {
-        if(name != null) {
-            this.name = name;
-        }
-    }
-
-    public int getHP() {
-        return this.hitPoint;
-    }
-
-    public void setHP(int hitPoint) {
-        if(hitPoint >= 0) {
-            this.hitPoint = hitPoint;
-        }
-    }
-    public int getattack() {
-        return this.attack;
-    }
-
-    public void setattack(int attack) {
-        if(attack >= 0) {
-            this.attack = attack;
-        }
-    }
-
-    public boolean getDeath() {
-        return this.dead;
-    }
-
-    public void setDeath(boolean dead) {
-        this.dead = dead;
-    }
-
-
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        if(this.dead == false){
-            int damage = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getname(), damage);
-            hero.wounded(damage);
-        }
+        super(name, maximumHP, attack);
     }
 
     /**
@@ -84,6 +24,7 @@ public class Enemy {
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
      * @param damage 受けたダメージ
      */
+    @Override
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
@@ -91,5 +32,4 @@ public class Enemy {
             System.out.printf("モンスター%sは倒れた。\n", name);
         }
     }
-
 }
